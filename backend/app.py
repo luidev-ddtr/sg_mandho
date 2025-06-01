debcc94c24e441a2ac01a04f1c890cadc3a4398e
luidev-ddtr <angeltepetate82@gmail.com>
2025-05-31 18:55:43 -0600

    sincroning changes, with server production and development Enviroment

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
 #Fichero el cual creara la instancia de flask # 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS  # Importa CORS
+
 #HARCODEAR LAS URL
 import os
+##Esto esta comentado ya que es solo de produccion
 
+#from dotenv import load_dotenv  # Importa load_dotenv
+# Carga el .env con ruta ABSOLUTA (crítico en PythonAnywhere)
+#load_dotenv('/home/luigas/sg_mandho/backend/.env')
 
 app = Flask(__name__)
 
@@ -23,6 +28,24 @@ CORS(app, resources={
         "supports_credentials": True  
     }
 })
+
+# @app.route("/", methos["GET"])
+# def index():
+#     return jsonfy({"messaje": "Hola desde flask inge ige"})
+string = "Hola mundo desde flask"
+
+@app.route('/')
+def home():
+    return jsonify({'message':"¡Funciona!",
+                    "index": {
+                        "nose": {
+                            "inge": [12,4,64,22,4,2,4],
+                            'valido': True,
+                        },
+                        "mañoso": len(string)
+                    },
+                    "final": ("si", True),
+                    }),200
 
 if __name__ == '__main__':
     app.run(debug=True)
